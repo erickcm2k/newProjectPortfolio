812b1ce97b109fdecc4159e439e74577c6e10bfb
Erick Castañeda Martínez <erickce40@gmail.com>
2020-11-15 00:06:32 -0600

    sticky navbar efect finished.

--- portfolio/src/Components/Navbar/Navbar.js
+++ portfolio/src/Components/Navbar/Navbar.js
@@ -1,30 +1,63 @@
 import React from "react";
 import "./Navbar.css";
 
-const NavBar = () => {
-  return (
-    <nav className="Navbar">
-      <ul className="List">
-        <li className="logo">
-          <a href="https://www.google.com">
-            <span>Erick Castañeda</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.google.com">About Me</a>
-        </li>
-        <li>
-          <a href="https://www.google.com">Projects</a>
-        </li>
-        <li>
-          <a href="https://www.google.com">Skills</a>
-        </li>
-        <li>
-          <a href="https://www.google.com">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      prevScrollpos: window.pageYOffset,
+      visible: true,
+    };
+  }
+
+  handleScroll = () => {
+    const { prevScrollpos } = this.state;
+    const currentScrollPos = window.pageYOffset;
+    let visible = true;
+    // Prevents problems with iOS overscroll
+    if (currentScrollPos > 0) {
+      visible = prevScrollpos > currentScrollPos;
+    }
+
+    this.setState({
+      prevScrollpos: currentScrollPos,
+      visible,
+    });
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  render() {
+    return (
+      <nav className={this.state.visible ? "Navbar" : "Navbar-hiden"}>
+        <ul className="List">
+          <li className="logo">
+            <a href="https://www.google.com">
+              <span>Erick Castañeda</span>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.google.com">About Me</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Projects</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Skills</a>
+          </li>
+          <li>
+            <a href="https://www.google.com">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 export default NavBar;
