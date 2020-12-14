db72d13787a09bb2a2119bb92412f22f3881b0dc
Erick Castañeda Martínez <erickce40@gmail.com>
2020-12-14 09:56:58 -0600

    detete old files

--- portfolio/src/Shared/Navigation/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Backdrop.scss";
-
-const Backdrop = (props) => {
-  return <div className="backdrop" onClick={props.click}></div>;
-};
-
-export default Backdrop;
